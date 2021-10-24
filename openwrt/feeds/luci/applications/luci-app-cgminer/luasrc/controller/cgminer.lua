70cbfdb8d96efbba10ec87bf2e08f4929885bc58
SaifAlSubhi <saifalsubhi@live.com>
2021-10-24 22:01:27 +0400

    Update cgminer.lua

--- openwrt/feeds/luci/applications/luci-app-cgminer/luasrc/controller/cgminer.lua
+++ openwrt/feeds/luci/applications/luci-app-cgminer/luasrc/controller/cgminer.lua
@@ -1,16 +1,5 @@
 --[[
 LuCI - Lua Configuration Interface
-
-Copyright 2014-2016 Mikeqin <Fengling.Qin@gmail.com>
-Copyright 2013 Xiangfu
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-$Id$
 ]]--
 
 module("luci.controller.cgminer", package.seeall)
